2d1f99cd1e21182452de63fbb8de394faa810b8f
SebastianHuss <Sebastian.Huss@proton.me>
2026-05-11 22:21:32 +0200

    added Circleshape and Player Classes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,13 +10,27 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # FPS festlegen
+    clock = pygame.time.Clock() # clock-Objekt erzeugen 
+    dt = 0 # Delta-Time festlegen
+
+    # Player-Object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # prüfen ob Fenster geschlossen wurde
                 return
-
+            
         screen.fill("black") # fill screen black
+        player.draw(screen)
+
+
         pygame.display.flip() # refresh screen
+        # Game-Loop Ende
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
